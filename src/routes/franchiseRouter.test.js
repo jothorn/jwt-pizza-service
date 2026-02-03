1a98c8f438a4f4df0898e4e28040342372507c7a
Jonathan Thornton <156238182+jothorn@users.noreply.github.com>
2026-02-03 15:01:01 -0700

    Create list franchises test

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -1,37 +1,58 @@
 const request = require("supertest");
 const app = require("../service");
-const { Role, DB } = require('../database/database.js');
+const { Role, DB } = require("../database/database.js");
 
 function randomName() {
-    return Math.random().toString(36).substring(2, 12);
+  return Math.random().toString(36).substring(2, 12);
 }
 
 async function createAdminUser() {
-    let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
-    user.name = randomName();
-    user.email = user.name + '@admin.com';
+  let user = { password: "toomanysecrets", roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + "@admin.com";
 
-    user = await DB.addUser(user);
-    return { ...user, password: 'toomanysecrets' };
+  user = await DB.addUser(user);
+  return { ...user, password: "toomanysecrets" };
 }
 
 let testUser;
 let testUserAuthToken;
 
 beforeAll(async () => {
-    testUser = await createAdminUser();
+  testUser = await createAdminUser();
 
-    const loginRes = await request(app).put("/api/auth").send(testUser);
-    testUserAuthToken = loginRes.body.token;
+  const loginRes = await request(app).put("/api/auth").send(testUser);
+  testUserAuthToken = loginRes.body.token;
 });
 
-test("create franchise", async () => {
-    const expectedFranchise = { name: randomName(), admins: [{ email: testUser.email }] }
-    const createFranchiseRes = await request(app)
-        .post(`/api/franchise`)
-        .set("Authorization", `Bearer ${testUserAuthToken}`)
-        .send(expectedFranchise);
-    // expect(createFranchiseRes.status).toBe(200);
+async function createFranchise() {
+  const expectedFranchise = {
+    name: randomName(),
+    admins: [{ email: testUser.email }],
+  };
+  const createFranchiseRes = await request(app)
+    .post("/api/franchise")
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send(expectedFranchise);
+  expect(createFranchiseRes.status).toBe(200);
 
-    expect(createFranchiseRes.body).toMatchObject(expectedFranchise);
+  expect(createFranchiseRes.body).toMatchObject(expectedFranchise);
+
+  return createFranchiseRes.body;
+}
+
+test("create franchise", createFranchise);
+
+test("list franchises", async () => {
+  const expected = await createFranchise();
+  const res = await request(app).get(
+    `/api/franchise?page-0&limit=1&name=${expected.name}`,
+  );
+
+  expect(res.status).toBe(200);
+  expect(res.body.franchises).toEqual(
+    expect.arrayContaining([
+      { id: expected.id, name: expected.name, stores: [] },
+    ]),
+  );
 });
