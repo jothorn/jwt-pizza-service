5252080ca92f6729699d843ba322fdec68fce631
Jonathan Thornton <156238182+jothorn@users.noreply.github.com>
2026-02-02 17:57:37 -0700

    Add update user test

--- src/routes/userRouter.test.js
+++ src/routes/userRouter.test.js
@@ -3,13 +3,14 @@ const app = require("../service");
 
 const testUser = { name: "pizza diner", email: "reg@test.com", password: "a" };
 let testUserAuthToken;
+let testUserId;
 
 beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + "@test.com";
   const registerRes = await request(app).post("/api/auth").send(testUser);
   testUserAuthToken = registerRes.body.token;
+  testUserId = registerRes.body.user.id;
 });
-
 
 test("get user", async () => {
   const getUserRes = await request(app)
@@ -23,3 +24,18 @@ test("get user", async () => {
   expect(getUserRes.body).toMatchObject(expectedUser);
 });
 
+test("update user", async () => {
+  const expectedUser = {
+    ...testUser,
+    roles: [{ role: "diner" }],
+    name: "new name",
+  };
+  const updateUserRes = await request(app)
+    .put(`/api/user/${testUserId}`)
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send(expectedUser);
+  expect(updateUserRes.status).toBe(200);
+
+  delete expectedUser.password;
+  expect(updateUserRes.body.user).toMatchObject(expectedUser);
+});
