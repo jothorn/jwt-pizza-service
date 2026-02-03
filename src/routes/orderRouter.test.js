6c7c5168917f3066604ab5917cab0941e8d51f8f
Jonathan Thornton <156238182+jothorn@users.noreply.github.com>
2026-02-03 16:57:22 -0700

    Test get pizza menu

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -5,6 +5,13 @@ const { randomName, createAdminUser } = require("./testingFunctions");
 let testUser;
 let testUserAuthToken;
 
+const item = {
+  title: "Student",
+  description: "No topping, no sauce, just carbs",
+  image: "pizza9.png",
+  price: 0.0001,
+};
+
 beforeAll(async () => {
   testUser = await createAdminUser();
 
@@ -12,17 +19,21 @@ beforeAll(async () => {
   testUserAuthToken = loginRes.body.token;
 });
 
-test("add an item to the menu", async () => {
-  const item = {
-    title: "Student",
-    description: "No topping, no sauce, just carbs",
-    image: "pizza9.png",
-    price: 0.0001,
-  };
+async function addItem() {
   const res = await request(app)
     .put("/api/order/menu")
     .set("Authorization", `Bearer ${testUserAuthToken}`)
     .send(item);
+
+  expect(res.status).toEqual(200);
+  expect(res.body).toContainEqual({ ...item, id: expect.any(Number) });
+}
+
+test("add an item to the menu", addItem);
+
+test("get the pizza menu", async () => {
+  await addItem();
+  const res = await request(app).get("/api/order/menu");
 
   expect(res.status).toEqual(200);
   expect(res.body).toContainEqual({ ...item, id: expect.any(Number) });
