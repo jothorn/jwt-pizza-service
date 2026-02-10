703e0ccff0a7da3e1f6155ba02017e69639fac4e
Jonathan Thornton <156238182+jothorn@users.noreply.github.com>
2026-02-09 21:54:12 -0700

    Add eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.node } },
   { languageOptions: { globals: globals.jest } },
   pluginJs.configs.recommended,
+  eslintConfigPrettier,
 ];
