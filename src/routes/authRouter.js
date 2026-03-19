9b5a5ba16d02b93227ebbb3005103ea65e4a900a
Jonathan Thornton <156238182+jothorn@users.noreply.github.com>
2026-03-19 14:56:25 -0600

    Increment auth attempts

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const config = require("../config.js");
 const { asyncHandler } = require("../endpointHelper.js");
 const { DB, Role } = require("../database/database.js");
+const metrics = require("../metrics.js");
 
 const authRouter = express.Router();
 
@@ -89,6 +90,7 @@ authRouter.post(
       roles: [{ role: Role.Diner }],
     });
     const auth = await setAuth(user);
+    metrics.trackAuthAttempt(true);
     res.json({ user: user, token: auth });
   }),
 );
@@ -98,9 +100,15 @@ authRouter.put(
   "/",
   asyncHandler(async (req, res) => {
     const { email, password } = req.body;
-    const user = await DB.getUser(email, password);
-    const auth = await setAuth(user);
-    res.json({ user: user, token: auth });
+    try {
+      const user = await DB.getUser(email, password);
+      const auth = await setAuth(user);
+      metrics.trackAuthAttempt(true);
+      res.json({ user: user, token: auth });
+    } catch (error) {
+      metrics.trackAuthAttempt(false);
+      throw error;
+    }
   }),
 );
 
